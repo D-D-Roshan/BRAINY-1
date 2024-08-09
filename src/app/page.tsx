92003d3d8bb0b7a9c518d9e3921fb83aca47562f
ROSHAN <113751850+D-D-Roshan@users.noreply.github.com>
2024-08-09 13:53:38 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+import Chattab from "@/components/Chattab";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Chattab/>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />
+      </Authenticated>
+    </div>
+  );
+}
